46f72707dbac10f471399ca1941d1c8a7288a5ee
Roberto B <roberto.butti@gmail.com>
2022-02-12 09:06:12 +0100

    "Catch All" route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome-laravel', function () {
     return view('welcome');
 });
+
+Route::get('{catchall}', function ($catchall) {
+    return 'Slug: '.$catchall;
+})->where('catchall', '.*');
+
